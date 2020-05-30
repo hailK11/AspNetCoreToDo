d670af4bd46d2d3f7df2f1afefb3344ab734b977
hailK11 <akhilkrishna.r2016@vitstudent.ac.in>
2020-05-30 12:37:06 +0530

    create a view model to hold ToDo items

---
+++ AspNetCoreToDo/Models/ToDoViewModel.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace AspNetCoreToDo.Models
+{
+    public class ToDoViewModel
+    {
+        public ToDoItem[] ToDoItems { get; set; }
+    }
+}
