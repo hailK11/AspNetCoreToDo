a3c06b2eee77da31ae9e2c910cdba76be4b759e5
hailK11 <akhilkrishna.r2016@vitstudent.ac.in>
2020-07-16 23:35:54 +0530

    wiring up service to the controller

--- AspNetCoreToDo/Controllers/ToDoController.cs
+++ AspNetCoreToDo/Controllers/ToDoController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AspNetCoreToDo.Services;
 
 namespace AspNetCoreToDo.Controllers
 {
     public class ToDoController : Controller
     {
-       public IActionResult Index()
+        private readonly IToDoItemService _todoItemService;
+
+        public ToDoController(IToDoItemService toDoItemService)
+        {
+            _todoItemService = toDoItemService;
+        }
+       public async Task<IActionResult> Index()
        {
-           
+           var items = await _todoItemService.GetIncompleteItemsAsync();
        } 
     }
 }
--- AspNetCoreToDo/Startup.cs
+++ AspNetCoreToDo/Startup.cs
@@ -12,6 +12,7 @@ using AspNetCoreToDO.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using AspNetCoreToDo.Services;
 
 namespace AspNetCoreToDO
 {
@@ -34,6 +35,8 @@ namespace AspNetCoreToDO
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
            services.AddRazorPages();
+           services.AddMvc();
+           services.AddSingleton<IToDoItemService, FakeToDoItemService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
